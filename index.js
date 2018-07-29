dd9fe318caa784b5fe19dd82c79f8ba80b91bf07
Best <rotimiibitoyebest@yahoo.co.uk>
2018-07-29 03:52:12 +0300

    Keep glitch awake

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ const bot = new Telegram.Telegram(process.env.API_KEY, {
     workers: 1
 });
 
+const http = require('http');
+const express = require('express');
+const app = express();
+
 const OtherwiseController = require('./controllers/otherwiseController')
 const BrainController = require('./controllers/brainController')
 const DictionaryController = require('./controllers/dictionaryController')
+
+app.get("index.js/", (request, response) => {
+    response.send(Date.now() + " Ping Received");
+    console.log(Date.now() + " Ping Received");
+    response.sendStatus(200);
+  });
+  app.listen(process.env.PORT, () => console.log('Server started'));
+  setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  }, 280000);
 
 bot.router
     .when(new TextCommand('/findbyword', 'wordSearchCommand'), new BrainController())
