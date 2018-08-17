ce75d14459e63149384677313bb4ae2fa3f28e2e
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2018-08-17 19:15:37 +0300

    Fixed bug with spellchecker for sentence

--- controllers/dictionaryController.js
+++ controllers/dictionaryController.js
@@ -11,6 +11,7 @@ const myChatId = '380473669';
 let smile = '🙂';
 let sad = '😞';
 let coolGlasses = '😎'
+let oneEye = '😉';
 let waitingSticker = 'CAADAgADPQgAAnlc4gkSO7rndkwKigI';
 
 class DictionaryController extends TelegramBaseController
@@ -28,15 +29,19 @@ class DictionaryController extends TelegramBaseController
             //Logic to suggest
             this.spellCheckerLogic($, word, user, userId, msg);
         } else if(msg == '/spellchecker'){
-            $.sendMessage(`*Send me the word you want to check its spelling.*\n\nI am waiting...${smile}`, {parse_mode: 'Markdown'});
-            $.sendSticker(`${waitingSticker}`);
+            $.sendMessage(`*Send me the WORD you want to check its spelling.*\n\nI am waiting...${smile}`, {parse_mode: 'Markdown'});
+            //$.sendSticker(`${waitingSticker}`);
             $.waitForRequest
                 .then($ => {
                     word = $.message.text;
-                    this.spellCheckerLogic($, word, user)
+                    if(word.length > 1 && word.match(/[a-z]/i)){
+                        this.spellCheckerLogic($, word, user, userId)
+                    } else {
+                        $.sendMessage(`Sorry your input is invalid, make sure you typed in english and its not a number.`, { parse_mode: "Markdown"})
+                        telegramBot.api.sendMessage(myChatId, `InvalidInputError[/spellchecker] =>\nUsername: ${user}\nUserId: ${userId}\nInput: ${msg}`)
+                    }
                 })
-
-        } else{
+        } else {
             //Sorry your word is invalid
             $.sendMessage(`Sorry your input is invalid, make sure you typed in english and its not a number.`, { parse_mode: "Markdown"})
             telegramBot.api.sendMessage(myChatId, `InvalidInputError[/spellchecker] =>\nUsername: ${user}\nUserId: ${userId}\nInput: ${msg}`)
@@ -50,7 +55,7 @@ class DictionaryController extends TelegramBaseController
         }
     }
 
-    spellCheckerLogic($, word, user) {
+    spellCheckerLogic($, word, user, userId) {
         let spellChecker = (err, dict) => {
         if(err) {
             throw err
@@ -63,23 +68,29 @@ class DictionaryController extends TelegramBaseController
             $.sendMessage(`Hey ${user}, your spelling is correct ${smile}, go ahead and find the meaning by just typing:\n\n/findbyword ${word}`, {parse_mode: 'Markdown'})
             telegramBot.api.sendMessage(myChatId, `User ${user} is using the spellCheckerHandler, but no suggestion for word: ${word}`);
         } else {
+          //Your word is not correct
             let suggestions = spellObj.suggest(word)
-            if(suggestions === Array){
+            if(Array.isArray(suggestions) && suggestions.length > 1){
                 $.sendMessage(this._serializeList(user, word, suggestions), {parse_mode: 'Markdown'})
+              telegramBot.api.sendMessage(myChatId, `User ${user} just used the spellChecker`) 
+            } else if( Array.isArray(suggestions) && suggestions.length === 1){
+                $.sendMessage(`Hey ${user}, ${word} is incorrect ${sad}. I got a suggestion${oneEye} for you:\n\n${suggestions[0]}.\n\nTo find the meaning just type: /findbyword ${suggestions[0]}`, {parse_mode: 'Markdown'})
+                telegramBot.api.sendMessage(myChatId, `User ${user} used the spellChecker`) 
             } else{
-                $.sendMessage(`Hey ${user}, the word ${word} is incorrect ${sad}. I got a suggestion${coolGlasses} for you:\n${suggestions[0]}.\nTo find the meaning just type: /findbyword ${suggestions[0]}`, {parse_mode: 'Markdown'})
+              $.sendMessage(`Unfortunately ${user}, that word isn't correct and I don't have any suggestion for you ${sad}.\n\nSince you are human you can correct it yourself${oneEye}. \n`)
+              telegramBot.api.sendMessage(myChatId, `InvalidInputError[/spellchecker] =>\nUsername: ${user}\nUserId: ${userId}\nInput: ${suggestions}`);
             }
         }
-        }
-        dictionary(spellChecker)
+      }
+      dictionary(spellChecker)
     }
 
     _serializeList(user, word, suggestions) {
-        let serialized = `*Hey ${user}, the word ${word} is incorrect ${sad}. I got some suggestions for you:* ${coolGlasses}\n\n`;
+        let serialized = `Hey ${user}, the word ${word} is incorrect ${sad}. I got some suggestions for you: ${oneEye}\n\n`;
         suggestions.forEach((suggestion) => {
             serialized +=  `${suggestion.charAt(0).toUpperCase() + suggestion.slice(1)}\n`
         })
-        serialized +=  `\nTo find the meaning of any of the word just type: /findbyword ${suggestions[0].charAt(0).toUpperCase()}`
+        serialized +=  `\nTo find the meaning of any of the word just type: /findbyword ${suggestions[0].charAt(0).toUpperCase() + suggestions[0].slice(1)}`
         return serialized;
     }
 }
