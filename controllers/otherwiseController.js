6ff41fdc4f7c8cc86c0404293f1f80a9e5a36770
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2018-08-20 18:39:57 +0300

    change help msg

--- controllers/otherwiseController.js
+++ controllers/otherwiseController.js
@@ -7,6 +7,7 @@ const telegramBot = require('../index.js')
 let coolGlasses = '😎'
 let oneEye = '😉';
 let fingerRight = '👉';
+let smile = '🙂';
 
 class OtherwiseController extends BaseController {
     /**
@@ -32,10 +33,9 @@ class OtherwiseController extends BaseController {
                 break;
             default:
                 //$.sendMessage(`To search for a word, e.g Bag\n\nJust type this:\n/findbyword Bag\n\nClick /help to see all I can do ${coolGlasses}`)
-                $.sendMessage(`To search for a word, e.g Bag\n\n1. Open the *MENU* (*it's an ICON on the top-right of your keyboard*)\n2. Click 🔎 Search\n3. Click ${fingerRight} /findbyword \n4. Enter the *WORD* you are looking for: Bag\n\nOR \n\n1. Click ${fingerRight} /findbyword \n2. Enter the *WORD* you are looking for.\n\n*If You Need Something Else You Can Always Find It In The Menu.*\Enjoy ${oneEye}`, { parse_mode: "Markdown" })
+                $.sendMessage(`To search for a word, e.g Bag\n\n1. Click ${fingerRight} /findbyword \n2. Enter the *WORD* you are looking for: Bag\n\nOR \n\n1. Open *MENU* (*The ICON on the top-right of your keyboard*)\n2. Click 🔎 Search\n3. Then follow the instruction ${smile}\n\n_If You Need Something Else You Can Always Find It In The Menu._\n*ENJOY* ${oneEye}`, { parse_mode: "Markdown" })
                 telegramBot.api.sendMessage(myChatId, `GeneralError =>\nUsername: ${firstName}\nUserId: ${userId}\nInput: ${msg}`)
         }
     }
 }
-
 module.exports = OtherwiseController
