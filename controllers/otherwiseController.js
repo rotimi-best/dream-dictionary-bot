a26b49a61d516d4ca8db695d404b5dfa43d1e77b
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2019-02-16 11:17:01 +0200

    exclude /start for otherwise controller

--- controllers/otherwiseController.js
+++ controllers/otherwiseController.js
@@ -22,18 +22,21 @@ class OtherwiseController extends BaseController {
     const firstName = $.message.chat.firstName || $.message.chat.lastName;
     const msg = $.message.text ? $.message.text : "Not a text";
     const userId = $.message.chat.id;
-    const testIfEng = RegExp('[a-z A-Z]');
+    const testIfEng = RegExp("[a-z A-Z]");
 
     if (testIfEng.test(msg)) {
-        brainController.wordSearchHandler($, "🔎 Search", msg);
+      brainController.wordSearchHandler($, "🔎 Search", msg);
 
-        API.sendMessage(ADMIN, `${firstName} Search from otherwise controller for ${msg}`);
-    } else {
-        $.sendMessage(
+      API.sendMessage(
+        ADMIN,
+        `${firstName} Search from otherwise controller for ${msg}`
+      );
+    } else if (msg !== "/start") {
+      $.sendMessage(
         `To search for a word, e.g Bag\n\n1. Open *MENU* (_The ICON on the top-right of your keyboard_)\n2. Click 🔎 Search\n3. Enter the *WORD* you are looking for: Bag\n\n_You can check your spelling, find synonyms and many more, just open the menu_${fingerDown} _and explore_\n*ENJOY* ${oneEye}`,
         { parse_mode: "Markdown" }
-        );
-        API.sendMessage(ADMIN, `404 from ${firstName} || ${userId}\n\n${msg}`);
+      );
+      API.sendMessage(ADMIN, `404 from ${firstName} || ${userId}\n\n${msg}`);
     }
 
     /*
