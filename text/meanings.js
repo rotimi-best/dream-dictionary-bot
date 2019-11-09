52c821bf0fd106af1a7740dd106ba2c27a2112f4
Best <irb.ossystem@gmail.com>
2019-11-09 13:12:15 +0200

    Add meanings for letter Y and Z

--- text/meanings.js
+++ text/meanings.js
@@ -1035,6 +1035,178 @@ const meanings = {
       "If seen in bad shape or broken in pieces, it is a bad omen against joy. Pray yourself out of likely sorrow on the way."
     ],
     bibleVerse: "1 Chronicles 16:42"
+  },
+  yacht: {
+    title: "YACHT",
+    introduction: "In the spirit yacht mean financial improvement.",
+    meanings: [
+      "If you dream being entertained on it, you are sure to realize your highest hopes.",
+      "To be on one in a choppy sea predicts sudden stroke of money luck.",
+      "To dream of one evacuated or burnt means ill-luck ahead."
+    ],
+    bibleVerse: "Hebrew 13:2."
+  },
+  yam: {
+    title: "YAM",
+    introduction:
+      "Yams are edible tubers grown mostly in tropical regions. They are kinds of tropical climbing plants. Yam tubers are round, short or long nature roots. The seeds are planted by first slicing them to sizes and bury them in the soil. It does well in loamy soil. Planted seeds (sliced tubers) germinate into full size of a tuber, after a period of eight or ten months. Long sticks are sunk into the ground beside it, to support the leaves' grow. The leaves are green at initial stage but turn brown at maturity. Brown leaves are signals of maturity for harvest. It is often wise to leave yams uncultivated until the leaves dry on the stem, if it is meant for storage. This will make it strong and mature. Yams don't do well in water-logged soil, else it becomes bad and rotten. The seeds are often white in color after peeling; few species have yellow colour as well. Yam tubers are either eaten cooked or roasted. Rodents and other related animals in the bush are its greatest enemies, as they eat the seeds raw. Yam tubers can be dried and or grinded into powder form as food. Its importance in the tropical region equates it to blessing and joy. In some localities, festivals are held when it is newly harvested. It is called yam festival.\n\nIn the spirit yam has meanings attach to it which we shall discuss below.\n1. The spiritual implication is, when you see good tubers ofyam in the dream, it foretells good luck and blessings.\n 2 To dream of bad, rotten or withered yam tubers suggests an upcoming period of difficulties, financial crisis or ill-health\n3. To dream of yams stored in barns portend handwork, success, prosperity and joy.\n\nIn the spirit yam mean blessings or joy.",
+    meanings: [
+      "To see good tubers mean good luck and blessings in the offing.",
+      "To dream of bad or rotten yam portends ill-health or financial breakdown."
+    ],
+    bibleVerse: "Proverbs 10:22"
+  },
+  yard: {
+    title: "YARD",
+    introduction:
+      "In the spirit yard represents joy, security, fear, captivity, as situation predicts, well kept one portends short time joy.",
+    meanings: [
+      "If you see a yard that is well kept with flowers it portends love and happiness.",
+      "To dream of being chained down in a yard portends captivity.",
+      "When you pass by a yard that has various people chained, but as soon as they saw you passed by, they all rose begging you to release them. It means you are into deliverance ministry, a crusader of repute, or an evangelist in the making. Obey the call of God upon your life."
+    ],
+    bibleVerse: ""
+  },
+  yardstick: {
+    title: "YARDSTICK",
+    introduction: "",
+    meanings: [
+      "A dream of this portends a challenge to your reputation. If broken, it predicts a season of general good luck."
+    ],
+    bibleVerse: ""
+  },
+  yarn: {
+    title: "YARN",
+    introduction: "",
+    meanings: [
+      "If you wind it into a ball in the dream it predicts new friends. Generally it portends, domestic happiness, and or romance. Really, the colour and details of the dream action must be correlated to give vivid interpretations. "
+    ],
+    bibleVerse: ""
+  },
+  yawn: {
+    title: "YAWN",
+    introduction: "",
+    meanings: [
+      "To do this or see someone in the act suggests that your tendency to negative thinking is holding you back. Endeavour to kick the habit one aside of you."
+    ],
+    bibleVerse: ""
+  },
+  yeast: {
+    title: "YEAST",
+    introduction: "In the spirit yeast portend wealth, abundance and joy ",
+    meanings: [
+      "To dream of one mean you should expect wealth from unexpected source."
+    ],
+    bibleVerse: "Exodus 12:15"
+  },
+  yellow: {
+    title: "YELLOW",
+    introduction:
+      "In the spirit yellow portends radiance, joy, light, gift from of God, marriage, family, honour, heated attitude and fear.",
+    meanings: [
+      "Yellow, sometimes forecasts setbacks and struggles before improvement can be achieved. Address your situation to this colour."
+    ],
+    bibleVerse: "Psalms 68:13, Proverbs 19:14, 2 Timothy. 17."
+  },
+  yodel: {
+    title: "YODEL",
+    introduction: "",
+    meanings: [
+      "To utter one or hear it in the dream, expect all your affairs to flourish"
+    ],
+    bibleVerse: ""
+  },
+  yoke: {
+    title: "YOKE",
+    introduction:
+      "To see a yoke of oxen in your dream forecasts a change of location.",
+    meanings: [
+      "To wear a yoke of any kind suggests you are allowing yourself to be dominated by others."
+    ],
+    bibleVerse: "1 John 5:3."
+  },
+  yucca: {
+    title: "YUCCA",
+    introduction:
+      "A dream featuring this lovely desert plant in bloom portends spiritual as well as material comforts of life.",
+    meanings: [""],
+    bibleVerse: ""
+  },
+  aqqqqq: {
+    title: "YOKE",
+    introduction: "",
+    meanings: [""],
+    bibleVerse: ""
+  },
+  zebra: {
+    title: "ZEBRA",
+    introduction: "In the spirit zebra connotes warning, or gain",
+    meanings: [
+      "If you dream of a herd of these, it is a warning that efforts are being expanded in the wrong direction.",
+      "If it is tame one, it predicts gain from unexpected source",
+      ""
+    ],
+    bibleVerse: "Jeremiah 6:6"
+  },
+  zero: {
+    title: "ZERO",
+    introduction: "In the spirit zero portends wasted energy",
+    meanings: [
+      "To dream of naught, reassess your objectives as it may lead you ta failure."
+    ],
+    bibleVerse: "Joel 1:10"
+  },
+  zinc: {
+    title: "ZINC",
+    introduction: "In the spirit zinc portends success or breakthrough",
+    meanings: [
+      "To dream of zinc partends solid achievement in all spheres of activity"
+    ],
+    bibleVerse: "Genesis 39:23."
+  },
+  zion: {
+    title: "ZION",
+    introduction: "In the spirit Zion portends church, safety, victory or joy",
+    meanings: [
+      "To be led, shown or guided to a place call Zion in the dream foretells divine intervention, kingship, elevation and victory."
+    ],
+    bibleVerse: "Psalms 2:6, Obadiah 1:17."
+  },
+  zipper: {
+    title: "ZIPPER",
+    introduction: "To dream of broken zipper means mistake or annoyance.",
+    meanings: [
+      "If it fastened easily, it predicts satisfaction in minor matters now pending."
+    ],
+    bibleVerse: ""
+  },
+  zircon: {
+    title: "ZIRCON",
+    introduction: "In the spirit Zircon portends confusion or error",
+    meanings: [
+      "To dream of this stone suggest misplaced confidence, and or, affection.",
+      "Re-examine your relationships with close ones. Keep your private affairs to yourself for meantime."
+    ],
+    bibleVerse: ""
+  },
+  zither: {
+    title: "ZITHER",
+    introduction:
+      "In the spirit Zither portends joy, peace of mind, happy social times with good friends. ",
+    meanings: [
+      "If this music instrument breaks or damage in the dream, it  portends sadness."
+    ],
+    bibleVerse: "Deuteronomy 16:15"
+  },
+  zoo: {
+    title: "ZOO",
+    introduction:
+      "Generic meaning to zoo, confusion, chaos, disarray, busy place, quality time.",
+    meanings: [
+      "To be at the zoo means you shall soon see faces in far off places.",
+      "To take child (or children) along means your travels will be profitable as well as pleasurable. When you are in one guarded by wild animals it foretells you are under strong captivity. Pray for liberation from barriers, marital warfare, poverty and sorrow."
+    ],
+    bibleVerse: "Luke 5:26, James 3:16"
   }
 };
 
