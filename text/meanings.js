73316578e5625a1f21261d79130af07a229cb3f8
Best <irb.ossystem@gmail.com>
2019-11-09 13:57:31 +0200

    feat: Add letter U text

--- text/meanings.js
+++ text/meanings.js
@@ -1,4 +1,121 @@
 const meanings = {
+  umbrella: {
+    title: "UMBRELLA",
+    introduction:
+      "An umbrella is a folding frame with a stick and handle, covered with cotton, silk, etc used to shelter the person holding it from rain or sun. An umbrella is sometimes used figuratively, as a means of protection. When it is said, Daddy serve as the umbrella for the family, means he is the breadwinner who everyone look up to. He serves as protector in time of trouble. He serves as refuge.\nAn umbrella can be used by two people at a time as a cover, one holding it for the other.The size of an umbrella determines its usefulness in relation to coverage.Small size umbrella can only cover the head of the holder.\nThus, it can not be used by two people at a time. Big size umbrella, can give shade for two people, giving protection to at least upper part of the body.In the spirit, an umbrella is most regarded as source of help, security and hope\n1. To dream of an umbrella portends symbol of security at hand.\n2. To see a broken or torn umbrella in the dream foretells warning of upcoming minor difficulties in your ventures.\n3. To hold one in the dream portends security. \n4.To dream of your umbrella being seïzed portends your undertakings are exposed to danger.",
+    meanings: [
+      "To see or hold an umbrella in the dream symbolizes security.",
+      "If you see broken or torn umbrella in the dream expect minor difficulties in your ventures.",
+      "When seized from you in the dream it means your undertakings are exposed to danger."
+    ],
+    bibleVerse: "Psalm 17:8."
+  },
+  ugly: {
+    title: "UGLY",
+    introduction:
+      "To dream of an ugly person portends luck. If you dream of being ugly in the dream it suggests you should cultivate a more outgoing personality if you want to hold on to valued friends. It also suggests to backslide. ",
+    meanings: [""],
+    bibleVerse: ""
+  },
+  uncle: {
+    title: "UNCLE",
+    introduction: "",
+    meanings: [
+      "To dream of your uncle is relative to money matters.",
+      "To see a dead uncle means financial difficulties in the offing."
+    ],
+    bibleVerse: "Genesis 27:43"
+  },
+  unconscious: {
+    title: "UNCONSCIOUS",
+    introduction: "",
+    meanings: [
+      "If you dream of someone else being unconscious it portends news of illness in the offing.",
+      "If you dream of yourself being unconscious it signifies a physical check up may do."
+    ],
+    bibleVerse: "Judges 4:19"
+  },
+  undertaker: {
+    title: "UNDERTAKER",
+    introduction: "",
+    meanings: [
+      "To see one stipulates surprise news of grief, and or untimely death."
+    ],
+    bibleVerse: ""
+  },
+  undertow: {
+    title: "UNDERTOW",
+    introduction: "",
+    meanings: [
+      "If caught in a dream of this backward wave, it portends you will soon have to comfort a sorrowing friend or relation."
+    ],
+    bibleVerse: ""
+  },
+  unicorn: {
+    title: "UNICORN",
+    introduction: "",
+    meanings: [
+      "To see this mythical creature in the dream portends you are in for a period of beneficial changes."
+    ],
+    bibleVerse: "Deuteronomy 33:17"
+  },
+  uniform: {
+    title: "UNIFORM",
+    introduction: "",
+    meanings: [
+      "If a man of God dream of wearing senior military uniform it signifies victory, and or, worldwide travels.",
+      "For a man to dream of wearing a uniform signifies approaching improvement in status.",
+      "If a woman dreamed of wearing one, it predicts happiness in love and in domestic affairs."
+    ],
+    bibleVerse: "2 Samuel 8:6."
+  },
+  university: {
+    title: "UNIVERSITY",
+    introduction: "",
+    meanings: [
+      "To dream of this institution portends future success to celebrate about."
+    ],
+    bibleVerse: ""
+  },
+  urine: {
+    title: "URINE",
+    introduction: "",
+    meanings: [
+      "The details of the dream action must be correlated. Really, to urinate in the dream is a symbol of increasing creative power and strength. If after you urinate in the dream, you felt uneasy it means delay and opposition to your progress. If you urinate ordinarily, it signifies release from tension and worry."
+    ],
+    bibleVerse: ""
+  },
+  upstair: {
+    title: "UPSTAIR",
+    introduction: "",
+    meanings: [
+      "To stay upstairs of a building portends strength, and or, good expectations. If ordered in the dream to pack your loads from upstairs to down stair of a building, it portends demotion."
+    ],
+    bibleVerse: "2 Samuel 11:2."
+  },
+  urn: {
+    title: "URN",
+    introduction: "",
+    meanings: [
+      "This vase filled with flow If the vase is filled with wise filled with flowers or plants predicts happiness.",
+      "If the vase is filled with withered flowers, it portends minor disappointments in the offing."
+    ],
+    bibleVerse: ""
+  },
+  usher: {
+    title: "USHER",
+    introduction: "",
+    meanings: [
+      "If you dreamed being an usher, or being ushered predicts new influential social contacts that could affect your future."
+    ],
+    bibleVerse: ""
+  },
+  aqqqqq: {
+    title: "",
+    introduction: "",
+    meanings: [""],
+    bibleVerse: ""
+  },
   vacation: {
     title: "VACATION",
     introduction: "",
@@ -1129,12 +1246,6 @@ const meanings = {
     title: "YUCCA",
     introduction:
       "A dream featuring this lovely desert plant in bloom portends spiritual as well as material comforts of life.",
-    meanings: [""],
-    bibleVerse: ""
-  },
-  aqqqqq: {
-    title: "YOKE",
-    introduction: "",
     meanings: [""],
     bibleVerse: ""
   },
